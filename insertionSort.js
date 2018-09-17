7a13ef293ce18503c5eaecb024d7f49ef0213180
MAwais <mawais54013@outlook.com>
2018-09-17 10:58:05 -0700

    add

---
+++ insertionSort.js
@@ -0,0 +1,33 @@
+var array = 40;
+
+var arr = [];
+for(var index = 0; index < array; index++)
+{
+    var randomNumber = Math.round(Math.random() * array);
+
+  arr.push(randomNumber);
+}
+
+function insertionSort(items)
+{
+    var i;
+    var j;
+
+    for(i = 0; i < items.length; i++)
+    {
+        var value = items[i];
+
+        for(j = i -1; j > -1 && items[j] > value; j--)
+        {
+            items[j + 1] = items[j];
+        }
+        items[j + 1] = value;
+    }
+    return items;
+}
+
+console.log("PRE-SORT");
+console.log(arr.join(" "));
+console.log("---------------------------");
+console.log("POST-SORT");
+console.log(insertionSort(arr).join(" "));
